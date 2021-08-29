42d6f870f2c4379a8efec51e2779f5e1c7620949
Nick Zabkar <nzabkar51@gmail.com>
2021-08-29 14:33:28 -0400

    Added employee constructor functon

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-inquirer.prompt([
-    {
-    name: "managerName",
-    type: "input",
-    message: "Who is managing this team?"
-    },
+const managerQuestions = () => {
+    return inquirer.prompt([
     
-    {
-    name: "managerId",
-    type: "input",
-    message: "What is the team managers employee ID?"
-    },
-    
-    {
-    name: "managerEmail",
-    type: "input",
-    message: "Please enter an email at which the team manager can be reached."
-    },
-    
-    {
-    name: "managerPhone",
-    type: "input",
-    message: "Please enter a contact phone number for the team manager"
-    },
+        {
+            name: "name",
+            type: "input",
+            message: "Who is managing this team?"
+        },
+        
+        {
+            name: "id",
+            type: "input",
+            message: "What is the team managers employee ID?"
+        },
+        
+        {
+            name: "email",
+            type: "input",
+            message: "Please enter an email at which the team manager can be reached."
+        },
+        
+        {
+            name: "office",
+            type: "input",
+            message: "Please enter the team manager's office number."
+        },
 
-]);
+        {
+            name: "add member",
+            type: "checkbox",
+            choices: ["Yes", "No"]
+        }
+    
+    ])
+    .then(managerData => {
+        const 
+    })
+
+    
+}
+
+managerQuestions();
+
