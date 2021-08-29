9f1020a1a23dc2cc91cb95cf29d3a3cbf5050892
Nick Zabkar <nzabkar51@gmail.com>
2021-08-28 20:48:57 -0400

    Created questions for manager profile

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Let's begin!");
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+    name: "managerName",
+    type: "input",
+    message: "Who is managing this team?"
+    },
+    
+    {
+    name: "managerId",
+    type: "input",
+    message: "What is the team managers employee ID?"
+    },
+    
+    {
+    name: "managerEmail",
+    type: "input",
+    message: "Please enter an email at which the team manager can be reached."
+    },
+    
+    {
+    name: "managerPhone",
+    type: "input",
+    message: "Please enter a contact phone number for the team manager"
+    },
+
+]);
