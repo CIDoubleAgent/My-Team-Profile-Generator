c6c221212822214b8d13ae4cf7440cbe7fe967d3
Nick Zabkar <nzabkar51@gmail.com>
2021-09-03 17:50:55 -0400

    Creating prompt workflow

--- index.js
+++ index.js
@@ -1,53 +1,29 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Manager = require("./lib/Manager");
+const managerPrompts = require('./lib/Prompts');
 
-let teamProfileArray = [];
-
-const managerQuestions = () => {
-    return inquirer.prompt([
-    
-        {
-            name: "name",
-            type: "input",
-            message: "Who is managing this team?"
-        },
-        
-        {
-            name: "id",
-            type: "input",
-            message: "What is the team managers employee ID?"
-        },
-        
-        {
-            name: "email",
-            type: "input",
-            message: "Please enter an email at which the team manager can be reached."
-        },
-        
-        {
-            name: "office",
-            type: "input",
-            message: "Please enter the team manager's office number."
-        },
-
-        {
-            name: "add member",
-            type: "checkbox",
-            choices: ["Yes", "No"]
-        }
-    
-    ])
-
-    .then(managerData => {
-        const { name, id, email, office } = managerData;
-        const manager = new Manager (name, id, email, office);
-        teamProfileArray.push(manager);
-        console.log(manager);
+function executePrompts(prompts, callback) {
+    return inquirer.prompt(prompts)
+    .then(responseData => {
+        callback(responseData)
     })
-
-    
 }
 
-managerQuestions();
+function handleManagerPrompts(promptData) {
+    let teamProfileArray = [];
+    const { name, id, email, office, add_member } = promptData;
+    const manager = new Manager (name, id, email, office);
+
+    teamProfileArray.push(manager.getData());
+
+    if (add_member === 'Yes') {
+
+    }
+}
+
+executePrompts(managerPrompts, handleManagerPrompts);
+
+
+
 
