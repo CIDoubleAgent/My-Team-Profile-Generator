2e794cf8ec5baa492bc3d66520884e899e24c06f
Nick Zabkar <nzabkar51@gmail.com>
2021-09-04 21:58:40 -0400

    Added html made in separate file

--- index.js
+++ index.js
@@ -47,53 +47,67 @@ function writeTeamMembersToFile(teamArray) {
     let html = ''
 
     html += `
-    <!DOCTYPE html>
-    <html lang="en-us">
-      <head>
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        
-        <title>My Team</title>
-        
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-        
+
+        <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
+        crossorigin="anonymous">
+            
         <link
         rel="stylesheet"
         href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-        crossorigin="anonymous"
-        />
-      </head>
-      <body>
-        <header>
-        
+        crossorigin="anonymous"/>
+
+        <title>My Team</title>
+    </head>
+
+    <body>
+        <header class= "w-100 py-5 mb-5 bg-danger text-white text-center">
+            <h1>My Team</h1>
         </header>
-        `
+
+        <div class= "w-100 mb-5 d-flex justify-content-center">
+            <section class= "d-flex flex-wrap justify-content-center" style= "max-width: 80%;">
+
+            `
 
     teamArray.forEach((person) => {
         let office = person.office ? person.office : ''
         let school = person.school ? person.school : ''
         let github = person.github ? person.github : ''
         html += `
-              <div class="team-member">
-                ${person.name}
-                ${person.role}
-                ${person.id}
-                ${person.email}
-                ${school}
-                ${office}
-                ${github}
-              </div>
-              `
+            <div class= "card m-3 bg-light shadow-lg" style= "width: 18rem;">
+                <h2 class= "ps-3 pt-2 mb-0 bg-primary text-white">Name</h2>
+                <h3 class= "ps-3 pt-2 pb-3 mb-0 bg-primary text-white">Role</h3>
+                <ul class="list-group px-3 py-5">
+                    <li class="list-group-item">ID: ${person.id}</li>
+                    <li class="list-group-item">Email: ${person.email}</li>
+                    <li class="list-group-item">EmlGitSch: ${office}${github}${school}</li>
+                </ul>
+            </div>`
     })
 
     html += `
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+        </section>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" 
+        integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" 
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" 
+        integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" 
+        crossorigin="anonymous"></script>
     </body>
-    </html>
-    `
+</html>
+`
     
     fs.writeFile("./dist/team.html", html, (err) => {
         console.log('File written!')
