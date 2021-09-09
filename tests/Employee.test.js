099004b7b19fe002fb578a7cea6a6f6a294afbf2
Nick Zabkar <nzabkar51@gmail.com>
2021-09-08 20:16:50 -0400

    Fixed Employee class test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require("../lib/Employee");
 
 it('creates a new employee object', () => {
-    const employee = new Employee('Nick', '1', 'nick@fakemail.com', 'Engineer', 'github', 'UNH');
-    expect(employee).toEqual({name: 'Nick', id: '1', email: 'nick@fakemail.com', role: 'Engineer', github: 'github', school:'UNH'});
+    const employee = new Employee('Nick', '1', 'nick@fakemail.com');
+    expect(employee).toEqual({name: 'Nick', id: '1', email: 'nick@fakemail.com'});
 })
